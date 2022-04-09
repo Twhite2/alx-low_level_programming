c2044a928124ca5f900621f5606d3b959537a1ce
Twhite2 <emmanuelmieye@gmail.com>
2022-04-08 18:54:11 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base 16 in lowercase.
+ * Can only use `putchar`, and only 3 times.
+ * Return: 0
+ */
+int main(void)
+{
+char hexc;
+int i;
+
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+
+for (hexc = 'a'; hexc < 'g'; hexc++)
+putchar(hexc);
+putchar('\n');
+
+return (0);
+}
