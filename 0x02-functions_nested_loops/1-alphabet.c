0b2f5949c6b1a8cae4ed46ac91814cb84997cf1c
Twhite2 <emmanuelmieye@gmail.com>
2022-04-12 14:44:48 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,16 +6,15 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void);
-int main(void)
-{
 void print_alphabet(void)
 {
+
 char alph = 'a';
 while (alph <= 'z')
+{
 _putchar(alph);
 alph++;
 _putchar('\n');
 }
-return (0);
+return;
 }
