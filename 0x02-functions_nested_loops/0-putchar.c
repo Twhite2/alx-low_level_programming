a0cacafb1695ed2833d0ebb8e90e46d9d96e410c
Twhite2 <emmanuelmieye@gmail.com>
2022-04-12 13:22:30 -0700

    tasko

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Write a program that prints `_putchar`
+ * followed by a new line
+ * cannot use printf or puts
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char pc = _putchar;
+
+putchar("%s\n", pc);
+
+return (0);
+}
