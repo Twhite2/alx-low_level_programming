6b3733d660ef6552e011167e34b99bb2eb83b1e6
Twhite2 <emmanuelmieye@gmail.com>
2022-04-12 14:38:52 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - prints alphabets in lowercase
+ *
+ * check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+int main(void)
+{
+void print_alphabet(void)
+{
+char alph = 'a';
+while (alph <= 'z')
+_putchar(alph);
+alph++;
+_putchar('\n');
+}
+return (0);
+}
