146231c63b13313a98b894475398853f23608d3a
Twhite2 <emmanuelmieye@gmail.com>
2022-04-14 11:57:00 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - Write a function
+ * that checks for upper case characters
+ * Return: 1 if `c` is upper, 0 if otherwise.
+ */
+int _isupper(int c)
+{
+char calph, salph;
+
+for (calph = 'A'; calph <= 'Z'; calph++)
+{
+if (calph <= 'Z')
+return 1;
+else
+return 0;
+}
+}
