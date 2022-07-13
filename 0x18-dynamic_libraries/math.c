0c95f1966573c48e77ac74803d9d990b06cdd65f
Twhite2 <emmanuelmieye@gmail.com>
2022-07-13 02:36:03 -0700

    task 2

---
+++ 0x18-dynamic_libraries/math.c
@@ -0,0 +1,30 @@
+#include "more.h"
+/**
+ * add, sub, mul, mod, div - functions in 100-operations.so library
+ * Return: functions
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
+
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+int div(int a, int b)
+{
+return (a / b);
+}
+
+int mod(int a, int b)
+{
+return (a % b);
+}
---
+++ 0x18-dynamic_libraries/more.h
@@ -0,0 +1,12 @@
+#ifndef _MORE_H_
+#define _MORE_H_
+
+#include <stdio.h>
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+#endif
